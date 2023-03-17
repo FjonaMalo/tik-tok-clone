4f965bfec05f0bbd69e696d120cffcfddf173813
fjonamalo <fjonamalo0@gmail.com>
2023-03-17 15:02:26 +0100

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDpupio_SlOswrmjmxiRRP2PvLPs5NolQo",
+  authDomain: "tik-tok-clone-1e390.firebaseapp.com",
+  projectId: "tik-tok-clone-1e390",
+  storageBucket: "tik-tok-clone-1e390.appspot.com",
+  messagingSenderId: "145862404219",
+  appId: "1:145862404219:web:1a919afe09428433d25ade",
+  measurementId: "G-BQ047ZTM69",
+};

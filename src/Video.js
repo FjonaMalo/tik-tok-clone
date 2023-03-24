69e4917d62c587c8b9520ac0a48e3bae8418be12
fjonamalo <fjonamalo0@gmail.com>
2023-03-24 15:41:35 +0100

    Working on video to play when we tap it

--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Video.css";
+import video1 from "./videos/video.mp4";
 
 function Video() {
+  const [playing, setPlay] = useState(false);
+  const videoRef = useRef(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  };
+
   return (
     <div className="video">
-      <h1>This is a video</h1>
+      <video
+        className="video__player"
+        loop
+        onClick={onVideoPress}
+        ref={videoRef}
+        controls
+        // autoPlay
+        src={video1}
+      ></video>
     </div>
   );
 }

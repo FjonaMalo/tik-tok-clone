830f31b047a5e7fe6f35215dc6aaddade21ce184
fjonamalo <fjonamalo0@gmail.com>
2023-03-24 17:56:21 +0100

    Finishesh Video Footer

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,22 +1,25 @@
 import React from "react";
 import "./VideoFooter.css";
-import Ticker from "react-ticker";
 import { MusicNote } from "@mui/icons-material";
 
-function VideoFooter() {
+function VideoFooter(channel, description, song) {
   return (
     <div className="videoFooter">
       <div className="videoFooter__text">
         <h3>@fjonamalo</h3>
-        <p>This is a description</p>
-        <MusicNote />
-        <Ticker mode="smooth">
-          {({ index }) => (
-            <>
-              <h1>Hello my friend</h1>
-            </>
-          )}
-        </Ticker>
+        <p>Checkout this video</p>
+        <div className="videofooter__ticker">
+          <MusicNote className="videoFooter__icon" />
+          {/* <Ticker mode="smooth">
+            {() => (
+              <>
+                <p>Hello my friend</p>
+              </>
+            )}
+            
+          </Ticker> */}
+          <marquee>The Chainsmokers - Closer</marquee>
+        </div>
       </div>
       <img
         className="videoFooter__record"

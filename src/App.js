93277151a4db72b1170df165360069e25b6d444d
fjonamalo <fjonamalo0@gmail.com>
2023-03-24 13:03:34 +0100

    Adding video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Video from "./Video";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build a tik tok clone</h1>
+      <div className="app__videos">
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
